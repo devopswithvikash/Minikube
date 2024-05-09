1a134fa1f25e1833358eaf77bb3c97a371bfa357
Ubuntu <azurevm@AzureVM-Large.eszoqkghubauhkc0x5rtdskicf.rx.internal.cloudapp.net>
2024-05-09 13:02:27 +0000

    Changed the Name of Script with question number of assignment

--- Script/4.sh
+++ Script/4.sh
@@ -6,6 +6,11 @@
 
 cd /home/azurevm/Helm
 
+#Create a helm chart and modify the Charts.yaml, values.yaml , templates/deployment.yaml and templates/service.yaml
+helm create my-redis-deployment
+
+#I have uploaded helm chart in the Github that contains the file
+
 # Deploy the Helm chart
 echo "Deploying Redis using Helm chart..."
 helm install my-redis .
